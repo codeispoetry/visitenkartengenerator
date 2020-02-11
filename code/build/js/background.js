bbc2ef0aa1b8cf94847dc7f3517882978e35bd28
Thomas Rose <t.rose@seidemann-web.com>
2020-02-11 17:37:09 +0100

    add background

--- code/build/js/background.js
+++ code/build/js/background.js
@@ -3,12 +3,16 @@ const background = {
     front: frontside.circle(0),
     back: backside.circle(0),
 
-    draw(){
+    draw() {
         background.front.remove();
         background.back.remove();
 
+        background.front = frontside.image("assets/bg_small.jpg", function () {
+           // background.front.size(frontside.width(), frontside.height());
+        });
 
-        background.front = frontside.rect( frontside.width() ,frontside.height()).fill("#46962b");
-        background.back = backside.rect( frontside.width() ,frontside.height()).fill("#46962b");
+        background.back = backside.image("assets/bg_small.jpg", function () {
+            // background.front.size(frontside.width(), frontside.height());
+        });
     }
 };
